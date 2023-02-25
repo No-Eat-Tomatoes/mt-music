0dabe3de46edfed07a0d4e3d5a40353592eb6259
曹养灿 <18953016423@163.com>
2023-02-25 13:24:08 +0800

    配置信息

---
+++ src/main/java/com/mt/music/MtMusicApplication.java
@@ -0,0 +1,16 @@
+package com.mt.music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author caoyc
+ * @date 2023/2/25
+ */
+@SpringBootApplication
+public class MtMusicApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MtMusicApplication.class, args);
+    }
+
+}
