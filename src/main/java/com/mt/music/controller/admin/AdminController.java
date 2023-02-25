96e07496a79f8436c79d440775487fa3dcb33c48
曹养灿 <18953016423@163.com>
2023-02-25 12:33:28 +0800

    测试提交

---
+++ src/main/java/com/mt/music/controller/admin/AdminController.java
@@ -0,0 +1,11 @@
+package com.mt.music.controller.admin;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author caoyc
+ * @date 2023/2/25
+ */
+@RequestMapping("/api/admin")
+public class AdminController {
+}
---
+++ src/main/java/com/mt/music/controller/user/UserController.java
@@ -0,0 +1,12 @@
+package com.mt.music.controller.user;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author caoyc
+ * @date 2023/2/25
+ */
+@RequestMapping("/api/user")
+public class UserController {
+}
+
